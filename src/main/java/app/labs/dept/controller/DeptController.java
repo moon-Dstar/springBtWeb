cc599a09ea0ee51de49402578bb0ef6a8a51b020
문준현 <jun8708@gmail.com>
2025-08-04 17:27:34 +0900

    8월 4일 commit
    REST API

--- src/main/java/app/labs/dept/controller/DeptController.java
+++ src/main/java/app/labs/dept/controller/DeptController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,6 +52,18 @@ public class DeptController {
 		return "dept/list";
 	}
 	
+	@GetMapping("list/{deptno}")
+	public String getDeptInfo(@PathVariable("deptno") int deptno, Model model) {
+		
+		List<Dept> deptList = deptService.getDeptList();
+		Dept dept = deptService.getDeptInfo(deptno);
+		
+		model.addAttribute("deptList", deptList);
+		model.addAttribute("dept", dept);
+		
+		return "dept/list";
+	}
+	
 	@PostMapping("insert")
 	public String insertDept(Dept dept, RedirectAttributes redirectAttributes) {
 		log.debug(dept.toString());
@@ -64,9 +77,40 @@ public class DeptController {
 		return "redirect:/dept/list";
 	}
 	
+	@PostMapping("update")
+	public String updateDept(Dept dept, RedirectAttributes redirectAttributes) {
+		log.debug(dept.toString());
+		
+		try {
+			deptService.updateDept(dept);
+			redirectAttributes.addFlashAttribute("message", dept.getDeptNo() + "번 부서가 수정되었습니다.");
+		} catch(RuntimeException e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+		}
+		return "redirect:/dept/list";
+	}
 	
-	
-	
+	@PostMapping("delete")
+	public String deleteDept(Dept dept, Model model, RedirectAttributes redirectAttributes) {
+		log.debug(dept.toString());
+		
+		try {
+			int cnt = deptService.deleteDept(dept.getDeptNo());
+			if (cnt > 0) {
+				redirectAttributes.addFlashAttribute("message", dept.getDeptNo() + "번 부서가 삭제되었습니다.");
+				return "redirect:/dept/list";
+			} else {
+				redirectAttributes.addFlashAttribute("message", "부서번호가 잘못되었습니다.");	
+				// model.addAttribute("dept", deptService.getDeptInfo(dept.getDeptNo()));
+				// return "dept/list";
+				return "redirect:/dept/list" + dept.getDeptNo();
+			}
+		} catch(RuntimeException e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+			return "redirect:/dept/list";
+		}
+		
+	}
 	
 	
 	
--- src/main/java/app/labs/dept/model/Dept.java
+++ src/main/java/app/labs/dept/model/Dept.java
@@ -1,5 +1,7 @@
 package app.labs.dept.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +15,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @Builder
 public class Dept {
+	// @JsonProperty("deptNo") // jackson => 첫번째 문자만 소문자로 변환
 	private int deptNo;
+	// @JsonProperty("dName") // dName => deptName 권장(DTO)
 	private String dName;
+	// @JsonProperty("loc")
 	private String loc;
 
 }
